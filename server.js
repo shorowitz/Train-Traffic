d0ba187a5b7cc73778b29a4457b3d4e092a8aae6
Sara Horowitz <sslhorowitz@gmail.com>
2016-02-28 17:57:25 -0500

    started to work with flexbox and css

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ var pgSession = require('connect-pg-simple')(session);
 
 var usersDB = require('./db/users_pg');
 var trainsDB = require('./db/trains_pg')
-var stopsDB = require('./db/stops_pg');
+
 var app = express();
 
 
 var userRoutes = require(path.join(__dirname, '/routes/users'));
 var trainRoutes = require(path.join(__dirname, '/routes/trains'))
-var stopRoutes = require(path.join(__dirname, '/routes/stops'));
+
 
 app.use(session({
   store: new pgSession({
@@ -49,11 +49,17 @@ app.set('view engine', 'ejs');
 // routes
 app.use('/users', userRoutes);
 app.use('/trains', trainRoutes);
-app.use('/stops',stopRoutes);
+
 
 app.get('/', function(req, res) {
   res.render('./pages/home.html.ejs', { user : req.session.user});
 });
+
+app.delete('/logout', function(req, res) {
+  req.session.destroy(function(err){
+    res.redirect('/');
+  })
+})
 
 var port = process.env.PORT;
 
