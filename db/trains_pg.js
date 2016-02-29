77badafd85d7dbefd34f15238fb0328f3888ac5b
Sara Horowitz <sslhorowitz@gmail.com>
2016-02-29 17:25:13 -0500

    comments view title functional

--- db/trains_pg.js
+++ db/trains_pg.js
@@ -30,12 +30,12 @@ function showStops(req,res,next){
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }
-    var query = client.query(`SELECT stops.name, stops.id
+    var query = client.query(`SELECT trains.name as trains_name, stops.name, stops.id
       FROM stops
       INNER JOIN trains
       ON stops.train_id = trains.id
       WHERE trains.id=$1
-      GROUP BY stops.name, stops.id
+      GROUP BY trains.name, stops.name, stops.id
       ORDER BY stops.id ASC;`, [req.params.id],
      function(err, results){
       done();
